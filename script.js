51c2d5c076df5a4f7947a8e8678d63f9f5768f3e
LayneHansen <layne.d.hansen@gmail.com>
2020-08-10 19:59:35 -0600

    created cards that contain text and styling

---
+++ script.js
@@ -0,0 +1,18 @@
+// Start Button
+
+// Timer
+    // create timer
+
+    // time decreases with wrong answer
+
+    // reaches zero, game over
+
+    // all questions answered, game over
+
+// Answer question with click
+
+    // switches to new question
+
+// Game Over! icon
+
+// Save score and initials
