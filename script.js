27b15a676ad370a341d7d3f1a910c996a18ec37b
LayneHansen <layne.d.hansen@gmail.com>
2020-08-14 14:46:35 -0600

    added functionality to the rest of questions, made visible in js

--- script.js
+++ script.js
@@ -17,14 +17,14 @@
     var start = document.querySelector("#start-button");
     var questionOne = document.getElementById("q1");
     var questionTwo = document.getElementById("q2");
-    // var questionThree = document.getElementById("q3");
-    // var questionFour = document.getElementById("q4");
-    // var questionFive = document.getElementById("q5");
-    // var questionSix = document.getElementById("q6");
-    // var questionSeven = document.getElementById("q7");
-    // var questionEight = document.getElementById("q8");
-    // var questionNine = document.getElementById("q9");
-    // var questionTen = document.getElementById("q10");
+    var questionThree = document.getElementById("q3");
+    var questionFour = document.getElementById("q4");
+    var questionFive = document.getElementById("q5");
+    var questionSix = document.getElementById("q6");
+    var questionSeven = document.getElementById("q7");
+    var questionEight = document.getElementById("q8");
+    var questionNine = document.getElementById("q9");
+    var questionTen = document.getElementById("q10");
     
 
     /* START BUTTON FUNCTION
@@ -47,12 +47,11 @@
         }, 1000);
         
         removeElement (start);
-        
         showElement (questionOne);
 
     });
 
-    // Event of answering question 1
+    // Event of answering questions
 
     // Question 1
 
@@ -66,34 +65,75 @@
 
         var quest2 = document.querySelector('.q2-correct');
         quest2.addEventListener('click', function() {
+            console.log(quest2);
             removeElement(questionTwo);
             showElement(questionThree);
+            console.log(questionThree);
+
         })
 
     // Question 3
+    
+        var quest3 = document.querySelector('.q3-correct');
+        quest3.addEventListener('click', function() {
+            removeElement(questionThree);
+            showElement(questionFour);
+
+        })
 
     // Question 4
+
+        var quest4 = document.querySelector('.q4-correct');
+        quest4.addEventListener('click', function() {
+            removeElement(questionFour);
+            showElement(questionFive);
     
     // Question 5
 
+        var quest5 = document.querySelector('.q5-correct');
+        quest5.addEventListener('click', function() {
+            removeElement(questionFive);
+            showElement(questionSix);
+
     // Question 6
+
+        var quest6 = document.querySelector('.q6-correct');
+        quest6.addEventListener('click', function() {
+            removeElement(questionSix);
+            showElement(questionSeven);
 
     // Question 7
 
+        var quest7 = document.querySelector('.q7-correct');
+        quest7.addEventListener('click', function() {
+            removeElement(questionSeven);
+            showElement(questionEight);
+
     // Question 8
 
+        var quest8 = document.querySelector('.q8-correct');
+        quest8.addEventListener('click', function() {
+            removeElement(questionEight);
+            showElement(questionNine);
+
     // Question 9
-    
+
+        var quest9 = document.querySelector('.q9-correct');
+        quest9.addEventListener('click', function() {
+            removeElement(questionNine);
+            showElement(questionTen);
+
     // Question 10
 
-    
-    
-    
+        var quest10 = document.querySelector('.q10-correct');
+        quest10.addEventListener('click', function() {
+            removeElement(questionTen);
+            showElement(questionThree);
 
-
+    
+    
     function removeElement (elementToRemove) {
         elementToRemove.style.display = 'none';
-
     }
 
     function showElement (elementToShow) {
@@ -125,16 +165,13 @@
 
 // Save score and initials
 
-/* save high scores
+// save high scores
 
-create function
+// create function
 
-function saveScore() {
-    scoreVariable
-    localStorage.setItem("scoreVariable", "High Score");
-}
+// function saveScore() {
+//     scoreVariable
+//     localStorage.setItem("scoreVariable", "High Score");
+// }
 
 // call function
-
-
-*/
