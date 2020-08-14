8f060a91b17ed4c3f1c59b4fff2fffc86ec18df5
LayneHansen <layne.d.hansen@gmail.com>
2020-08-13 22:37:20 -0600

    functionality to questions 1 and 2

--- script.js
+++ script.js
@@ -10,12 +10,29 @@
 
     // TIMER
 
-    // var startTime = document.getElementById('#start-button');
     var secondsDisplay = document.querySelector("#seconds");
 
     var secondsRemain = 60;
 
     var start = document.querySelector("#start-button");
+    var questionOne = document.getElementById("q1");
+    var questionTwo = document.getElementById("q2");
+    // var questionThree = document.getElementById("q3");
+    // var questionFour = document.getElementById("q4");
+    // var questionFive = document.getElementById("q5");
+    // var questionSix = document.getElementById("q6");
+    // var questionSeven = document.getElementById("q7");
+    // var questionEight = document.getElementById("q8");
+    // var questionNine = document.getElementById("q9");
+    // var questionTen = document.getElementById("q10");
+    
+
+    /* START BUTTON FUNCTION
+        1. Push Button
+        2. Button Disappears
+        3. Timer Starts
+        4. First question appears
+    */
 
     start.addEventListener("click", function () {
         var secondsInterval = setInterval(function () {
@@ -31,11 +48,48 @@
         
         removeElement (start);
         
-        var questionOne = document.getElementById("q1");
         showElement (questionOne);
 
-
     });
+
+    // Event of answering question 1
+
+    // Question 1
+
+        var quest1 = document.querySelector('.q1-correct');
+        quest1.addEventListener('click', function() {
+            removeElement(questionOne);
+            showElement(questionTwo);
+        })
+
+    // Question 2
+
+        var quest2 = document.querySelector('.q2-correct');
+        quest2.addEventListener('click', function() {
+            removeElement(questionTwo);
+            showElement(questionThree);
+        })
+
+    // Question 3
+
+    // Question 4
+    
+    // Question 5
+
+    // Question 6
+
+    // Question 7
+
+    // Question 8
+
+    // Question 9
+    
+    // Question 10
+
+    
+    
+    
+
 
     function removeElement (elementToRemove) {
         elementToRemove.style.display = 'none';
@@ -45,6 +99,8 @@
     function showElement (elementToShow) {
         elementToShow.style.display = 'block';
     }
+
+
 
 
     // time decreases with wrong answer
@@ -68,3 +124,17 @@
 // Game Over! icon
 
 // Save score and initials
+
+/* save high scores
+
+create function
+
+function saveScore() {
+    scoreVariable
+    localStorage.setItem("scoreVariable", "High Score");
+}
+
+// call function
+
+
+*/
